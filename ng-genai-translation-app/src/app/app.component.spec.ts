4abd51252cbe47c9cf1d639c146e679e5c122f89
Connie Leung <cleung334@gmail.com>
2024-03-29 02:45:43 +0800

    feat: create angular app

--- ng-genai-translation-app/src/app/app.component.spec.ts
+++ ng-genai-translation-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ng-translation-app' title`, () => {
+  it(`should have the 'ng-genai-translation-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-translation-app');
+    expect(app.title).toEqual('ng-genai-translation-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-translation-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-genai-translation-app');
   });
 });
--- ng-genai-translation-app/src/app/app.component.ts
+++ ng-genai-translation-app/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng-translation-app';
+  title = 'ng-genai-translation-app';
 }
