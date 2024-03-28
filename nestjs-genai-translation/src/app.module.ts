c603fff3cdc28dac001b2425dd6089d0b71f8c04
Connie Leung <cleung334@gmail.com>
2024-03-29 01:41:07 +0800

    refactor: delete status methods in TranslationModule

--- nestjs-genai-translation/src/app.module.ts
+++ nestjs-genai-translation/src/app.module.ts
@@ -4,14 +4,12 @@ import { ThrottlerGuard } from '@nestjs/throttler';
 import { env } from '~configs/env.config';
 import { throttlerConfig } from '~configs/throttler.config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TranslationModule } from './translation/translation.module';
 
 @Module({
   imports: [throttlerConfig, TranslationModule.register(env.AI_SERVICE)],
   controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
--- nestjs-genai-translation/src/translation/translation.module.ts
+++ nestjs-genai-translation/src/translation/translation.module.ts
@@ -11,6 +11,33 @@ import { GOOGLE_TRANSLATE_PROVIDER } from './application/providers/google-transl
 import { GEMINI_LLM_CHAIN_PROVIDER } from './application/providers/translation-chain.provider';
 import { TranslatorController } from './presenters/http/translator.controller';
 
+function getProviders(isProduction: boolean, serviceType: Integration) {
+  const translatorService = getService(isProduction, serviceType);
+  const providers: Provider[] = [
+    {
+      provide: TRANSLATOR,
+      useClass: translatorService,
+    },
+  ];
+
+  if (serviceType === 'langchain_googleChatModel') {
+    providers.push(GEMINI_LLM_CHAIN_PROVIDER);
+  } else if (serviceType === 'google_translate') {
+    providers.push(GOOGLE_TRANSLATE_PROVIDER);
+  }
+  return providers;
+}
+
+function getService(isProduction: boolean, serviceType: Integration) {
+  const serviceMap = new Map<Integration, any>();
+  serviceMap.set('azureOpenAI', AzureTranslatorService);
+  serviceMap.set('langchain_googleChatModel', LangchainTranslatorService);
+  if (!isProduction) {
+    serviceMap.set('google_translate', GoogleTranslateService);
+  }
+  return serviceMap.get(serviceType);
+}
+
 @Module({
   imports: [HttpModule],
   controllers: [TranslatorController],
@@ -20,38 +47,11 @@ export class TranslationModule {
     const isProduction = env.APP_ENV === APP_ENV_NAMES.PRODUCTION;
     // google_translation works in local environment. Default to azureOpenAI in production
     const serviceType = isProduction && type === 'google_translate' ? 'azureOpenAI' : type;
-    const providers = TranslationModule.getProviders(isProduction, serviceType);
+    const providers = getProviders(isProduction, serviceType);
 
     return {
       module: TranslationModule,
       providers,
     };
-  }
-
-  private static getProviders(isProduction: boolean, serviceType: Integration) {
-    const translatorService = TranslationModule.getService(isProduction, serviceType);
-    const providers: Provider[] = [
-      {
-        provide: TRANSLATOR,
-        useClass: translatorService,
-      },
-    ];
-
-    if (serviceType === 'langchain_googleChatModel') {
-      providers.push(GEMINI_LLM_CHAIN_PROVIDER);
-    } else if (serviceType === 'google_translate') {
-      providers.push(GOOGLE_TRANSLATE_PROVIDER);
-    }
-    return providers;
-  }
-
-  private static getService(isProduction: boolean, serviceType: Integration) {
-    const serviceMap = new Map<Integration, any>();
-    serviceMap.set('azureOpenAI', AzureTranslatorService);
-    serviceMap.set('langchain_googleChatModel', LangchainTranslatorService);
-    if (!isProduction) {
-      serviceMap.set('google_translate', GoogleTranslateService);
-    }
-    return serviceMap.get(serviceType) || LangchainTranslatorService;
   }
 }
